31f11e953e0d0da1859c7fee2d2b28338baa7bf7
codemakim <kaeaoga@gmail.com>
2019-02-01 03:44:27 +0900

    레이아웃 삽질중

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,16 @@
 <template>
-    <header class="w3-bar w3-amber">
-        header
+    <header class="w3-bar w3-card">
+        <button v-on:click="showSidebar()" class="w3-button w3-white">☰</button>
     </header>
 </template>
 
 <script>
 export default {
-    
+    methods: {
+        showSidebar() {
+            document.querySelector('nav.w3-sidebar').style.display = 'block';
+        }
+    }
 }
 </script>
 
--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,18 +1,23 @@
 <template>
-    <nav class="w3-sidebar w3-indigo w3-bar-block w3-collapse">
+    <nav class="w3-sidebar w3-bar-block w3-collapse w3-card">
+        <button v-on:click="closeSidebar()" class="w3-hide-large w3-button w3-block">Close(X)</button>
         SideBar
     </nav>
 </template>
 
 <script>
 export default {
-    
+    methods: {
+        closeSidebar() {
+            document.querySelector('nav.w3-sidebar').style.display = 'none';
+        }
+    }
 }
 </script>
 
 <style scoped>
     nav.w3-sidebar {
         width: 25%;
-        max-width: 320px;
+        width: 320px;
     }
 </style>
