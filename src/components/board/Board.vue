3f526af546c3cdff50199145ff8ce9b1d34ce38a
codemakim <kaeaoga@gmail.com>
2019-09-09 02:09:36 +0900

    이것도! 성공!

--- src/components/board/Board.vue
+++ src/components/board/Board.vue
@@ -2,28 +2,31 @@
   <div class="w3-container">
     <h2>게시판 제목</h2>
     <!-- 이 아래 컴포넌트들이 라우터 뷰 영역 내에서 선택되어 나타날 것입니다. -->
+    <router-view></router-view>
+    <!-- 
     <Main></Main>
     <Detail></Detail>
     <Write></Write>
     <List></List>
     <pagination></pagination>
+     -->
   </div>
 </template>
 
 <script>
-import Main from "./Board_main.vue";
-import List from "./Board_list.vue";
-import Detail from "./Board_view.vue";
-import Write from "./Board_write.vue";
-import Pagination from "./Board_pagination.vue";
+// import Main from "./Board_main.vue";
+// import List from "./Board_list.vue";
+// import Detail from "./Board_view.vue";
+// import Write from "./Board_write.vue";
+// import Pagination from "./Board_pagination.vue";
 export default {
-  components: {
-    Main,
-    List,
-    Detail,
-    Write,
-    Pagination
-  }
+  // components: {
+  //   Main,
+  //   List,
+  //   Detail,
+  //   Write,
+  //   Pagination
+  // }
 };
 </script>
 
--- src/components/board/Board_main.vue
+++ src/components/board/Board_main.vue
@@ -3,7 +3,7 @@
     <div class="w3-row-padding">
       <div v-for="n in 3" v-bind:key="n" class="w3-col m12 l6">
         <div class="w3-card w3-padding board-main-item">
-          <h4>게시판 제목1</h4>
+          <router-link to="/board" tag="h4">게시판 제목 {{n}}</router-link>
           <ol>
             <li v-for="n in 6" v-bind:key="n">탑게시물입니다.</li>
           </ol>
